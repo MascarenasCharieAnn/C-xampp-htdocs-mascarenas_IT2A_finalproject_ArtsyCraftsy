531c696b8862de1dd8c6b87765fc933b6b8e8d0c
MascarenasCharieAnn <charieannmascarenas@gmail.com>
2022-05-15 23:03:23 +0800

    About

---
+++ About.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible"content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>ArtsyCraftsy</title>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="css/mycss.css" rel="stylesheet">
+
+
+  <!-- Bootstrap core JS-->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+
+</head>
+<body>
+  <div class="container shadow-lg py-1">
+  
+
+   <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="me-md-auto">
+         
+      </a>
+
+      <ul class="nav nav-pills fw-bolder text-dark">
+        <li class="nav-item "><a href="Home.php" class="nav-link">Home</a></li>
+        <li class="nav-item "><a href="Gallery.php" class="nav-link">Gallery</a></li>
+        <li class="nav-item "><a href="About.php" class="nav-link active">About</a></li>
+      </ul>
+    </header>
+   
+    <?php echo "<img src='images/Logo.png' class='img-fluid img-responsive d-block mx-auto' alt='Responsive image'>" ?>
+    <h5 class="text-center py-4 px-4">
+    <?php echo "ArtsyCraftsy is your ultimate source, dedicated to providing art lovers with the best source of the Famous Paintings Of All Time In The History Of Art. I hope you appreciate the most excellent paintings, the greatest hand-paintings by our famous artists." ?></h5>
+
+
+    <p class="text-center px-4">
+    <?php echo 
+    "If you have any remarks or questions about the website don’t hesitate to contact us through mail at:" ?>
+    </p>
+    <p class="fw-bolder text-center">ArtsyCraftsy@gmail.com</p>
+
+     
+
+  <footer class="border-top bg-danger">
+        <p class="text-center py-1 text-light">
+          <?php echo "Copyright &copy; 2022. Charie Ann Mascarenas & Rissha Angeline Jalotjot" ?></p>
+    </footer>
+
+   </div>
+   
+</body>
+</html>
