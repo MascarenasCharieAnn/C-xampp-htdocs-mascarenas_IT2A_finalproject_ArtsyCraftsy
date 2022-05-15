95ddda5467d697b8c039b4f42b4d3bf4cf66a7b2
MascarenasCharieAnn <charieannmascarenas@gmail.com>
2022-05-15 22:56:21 +0800

    Gallery

---
+++ Gallery.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible"content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<title>ArtsyCraftsy</title>
+
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<link href="css/mycss.css" rel="stylesheet">
+
+
+	<!-- Bootstrap core JS-->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+
+</head>
+<body>
+	<div class="container shadow-lg py-1">
+	
+	 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+  		
+  		<a href="/" class="me-md-auto">
+        <?php echo "<img src='images/Logo.png' alt='Responsive image' class='icon-header img-fluid'>" ?>
+      </a>
+
+      <ul class="nav nav-pills fw-bolder text-dark">
+        <li class="nav-item "><a href="Home.php" class="nav-link">Home</a></li>
+        <li class="nav-item "><a href="Gallery.php" class="nav-link active">Gallery</a></li>
+        <li class="nav-item "><a href="About.php" class="nav-link">About</a></li>
+      </ul>
+
+    </header>
+   
+    <div class="">
+
+    
+    <main>
+  <h1 class="fw-bolder px-4 py-4 text-center text-secondary display-4"><?php echo"Gallery of 12 Most Famous Paintings of All Time"?></h1>
+  <div class="album py-5 bg-light">
+    <div class="container">
+       
+  
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_1.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "The Mona Lisa" ?></h3>
+              <p class="card-text"><?php echo "Italian noblewoman Lisa Del Giocondo, countless hypotheses have been put forth as to the sitter’s identity as well as explanations for..."?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                 <a type="button"  href="The Mona Lisa.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_2.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "Starry Night" ?></h3>
+              <p class="card-text"><?php echo "Van Gogh was seeking respite from plaguing depression at the Saint-Paul asylum in Saint-Rémy in southern France when he painted..." ?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="Starry Night.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_3.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "Girl with a Pearl Earring" ?></h3>
+              <p class="card-text text-justify"><?php echo " “Girl with a Pearl Earring” has transfixed viewers with her wistful gaze ever since the painting..." ?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="Girl with a Pearl Earring.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_4.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "American Gothic" ?></h3>
+              <p class="card-text text-justify"><?php echo "A national icon and leading exponent of regionalism, “American Gothic” depicts what appears to be a Depression-era farmer and his weathered..."?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="American Gothic.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_5.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "Arnolfini Portrait" ?></h3>
+              <p class="card-text text-justify"><?php echo "The elegantly dressed couple are thought to be Giovanni di Nicolao di Arnolfini, and his wife, Costanza Trenta, wealthy Italians living in Bruges. The unusual composition..." ?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="Arnolfini Portrait.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_6.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "Le Déjeuner sur l'herbe" ?></h3>
+              <p class="card-text text-justify"><?php echo "Edouard Manet’s sensational “Le Déjeuner sur l'herbe” (“The Luncheon on the Grass”) scandalized 19th-century Paris, not for its stark nudity, but because..." ?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="Le Déjeuner sur l'herbe.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_7.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "Christina’s World" ?></h3>
+              <p class="card-text text-justify"><?php echo "Christina’s World” continues to fascinate more than 70 years after it was first painted. The faceless woman lying on the ground was Anna Christina Olson, the neighbor and muse of Pennsylvania artist Andrew Wyeth..."?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="Christina’s World.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_8.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "The Persistence of Memory" ?></h3>
+              <p class="card-text text-justify"><?php echo "Surrealist Salvador Dali subverts reality with this mesmerizing image of deflated timepieces scattered over a desert landscape..."?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="The Persistence of Memory.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_9.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "The Creation of Adam" ?></h3>
+              <p class="card-text text-justify"><?php echo "The Creation of Adam is a detailed, incredible work that can be interpreted in many different ways. The image has ..."?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="The Creation of Adam.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_10.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "Flaming June" ?></h3>
+              <p class="card-text text-justify"><?php echo "Flaming June,” of the languid beauty in the transparent orange dress, was painted by esteemed British artist Frederic Leighton at the close of the 19th century..."?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="Flaming June.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo"<img src='images/artwork_11.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>" ?>
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "Primavera" ?></h3>
+              <p class="card-text text-justify"><?php echo "Primavera is an allegorical work inspired by classical mythology, depicting the transformation of the nymph Chloris into Flora, the goddess of spring..." ?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="Primavera.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+                <title>Placeholder</title><rect width="100%" /><?php echo "<img src='images/artwork_12.jpg' class='img-fluid d-block mx-auto py-3' alt='Responsive image'>"?>
+
+            <div class="card-body">
+              <h3 class="card-title fw-bolder"><?php echo "The Son of Man" ?></h3>
+              <p class="card-text text-justify"><?php echo "The meaning of the painting is cryptic, but one can extrapolate some meaning from the words of Magritte himself. He spoke specifically of that feeling of..." ?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a type="button"  href="The Son of Man.php" class="btn btn-sm btn-outline-primary">View</a> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+</main>
+
+   </div>
+
+	<footer class="border-top bg-danger">
+   			<p class="text-center py-1 text-light">
+         <?php echo "Copyright &copy; 2022. Charie Ann Mascarenas & Rissha Angeline Jalotjot" ?></p>
+  	</footer>
+
+   </div>
+</body>
+</html>
